ea258513ec539094c11d17dc795cedce7962b551
IceWaitSnow <98445415+IceWaitSnow@users.noreply.github.com>
2026-06-15 17:53:50 +0800

    Add shared spline tool version constant

---
+++ Assets/VFXTimelineSplineTool/Runtime/VFXSplineToolVersion.cs
@@ -0,0 +1,9 @@
+namespace VFXTimelineSplineTool
+{
+    public static class VFXSplineToolVersion
+    {
+        public const string Version = "2.6.4";
+        public const string DisplayName = "VFX Timeline Spline Tool";
+        public const string FullName = DisplayName + " v" + Version;
+    }
+}
